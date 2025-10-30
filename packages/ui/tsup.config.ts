8bbfb6595e9d55a427aed09d1c297e598b55859a
BroderickMitchell <bmitchell@workrightadvisory.com.au>
2025-10-30 19:15:39 +0800

    chore: fix ui build configuration

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/tailwind.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
+  entry: ["src/index.ts", "src/tailwind.ts"],
+  format: ["cjs", "esm"],
+  dts: { resolve: true },
   splitting: false,
   sourcemap: true,
   clean: true,
   shims: true,
-  external: ['react', 'react-dom', 'next']
+  target: "es2020",
+  tsconfig: "tsconfig.json",
+  external: ["react", "react-dom", "next"],
 });
